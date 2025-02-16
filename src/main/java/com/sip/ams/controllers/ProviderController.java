9f2a7d4cf14dc5bae1cacd19418fa595c748483c
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-02-16 15:33:47 +0100

    ajout de cross origin

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import java.time.LocalDateTime;
 import java.util.logging.*;
 @RestController
 @RequestMapping("/providers")
+@CrossOrigin("*")
 public class ProviderController {
 
 	private final ProviderService providerService;
